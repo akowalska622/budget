195f0c4141f4fc6404299e7f87a8f7ee72da9494
Anna Kowalska <a.kowalska622@gmail.com>
2021-07-16 21:59:01 +0200

    form

--- src/components/Movements/AddNew.js
+++ src/components/Movements/AddNew.js
@@ -1,13 +1,12 @@
 import { Fragment, useState } from 'react';
 import classes from './AddNew.module.css';
-import Button from '../UI/Button';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 const AddNew = () => {
   const [isFormShown, setIsFormShown] = useState(false);
 
-  const initialValues = { type: '', title: '', amount: '' };
+  const initialValues = { type: 'income', title: '', amount: '', category: '' };
 
   const validation = Yup.object({
     title: Yup.string()
@@ -21,14 +20,17 @@ const AddNew = () => {
     type: Yup.string()
       .oneOf(['income', 'expense'], 'Invalid type')
       .required('Type required'),
+
+    category: Yup.string().required('Category required'),
   });
 
   const expandForm = () => {
     setIsFormShown(prevState => !prevState);
   };
 
-  const handleSubmit = values => {
+  const handleSubmit = (values, { resetForm }) => {
     alert(JSON.stringify(values, null, 2));
+    resetForm();
   };
 
   return (
@@ -45,30 +47,86 @@ const AddNew = () => {
           validationSchema={validation}
           onSubmit={handleSubmit}
         >
-          <Form className={classes.form}>
-            <div className={classes.formControl}>
-              <label htmlFor='title'>Title</label>
-              <Field className={classes.input} name='title' type='text' />
-              <ErrorMessage
-                className={classes.error}
-                component='small'
-                name='title'
-              />
-            </div>
+          {({ values }) => (
+            <Form className={classes.form}>
+              <div className={classes.radioBtn} role='group'>
+                <label>
+                  <Field type='radio' name='type' value='income' />
+                  Income
+                </label>
+                <label>
+                  <Field type='radio' name='type' value='expense' />
+                  Expense
+                </label>
+                <ErrorMessage name='type' />
+              </div>
+              <div className={classes.formControl}>
+                <label htmlFor='title'>Title</label>
+                <Field className={classes.input} name='title' type='text' />
+                <ErrorMessage
+                  className={classes.error}
+                  component='small'
+                  name='title'
+                />
+              </div>
 
-            <div className={classes.formControl}>
-              <label htmlFor='amount'>Amount</label>
-              <Field className={classes.input} name='amount' type='number' />
-              <ErrorMessage
-                className={classes.error}
-                component='small'
-                name='amount'
-              />
-            </div>
-            <button className={`${classes.btn} ${classes.btnSubmit}`}>
-              Submit
-            </button>
-          </Form>
+              <div className={classes.formControl}>
+                <label htmlFor='amount'>Amount</label>
+                <Field className={classes.input} name='amount' type='number' />
+                <ErrorMessage
+                  className={classes.error}
+                  component='small'
+                  name='amount'
+                />
+              </div>
+              {values.type === 'income' && (
+                <div className={classes.formControl}>
+                  <label htmlFor='category'>Category</label>
+                  <Field className={classes.input} as='select' name='category'>
+                    <option value=''>-- select an option --</option>
+                    <option value='salary'>Salary</option>
+                    <option value='gift'>Gift</option>
+                    <option value='freelancing'>Freelancing</option>
+                    <option value='savings'>Savings</option>
+                    <option value='prize'>Prize</option>
+                    <option value='trade'>Trade</option>
+                  </Field>
+                  <ErrorMessage
+                    className={classes.error}
+                    component='small'
+                    name='category'
+                  />
+                </div>
+              )}
+              {values.type === 'expense' && (
+                <div className={classes.formControl}>
+                  <label htmlFor='category'>Category</label>
+                  <Field className={classes.input} as='select' name='category'>
+                    <option value=''>-- select an option --</option>
+                    <option value='food'>Food</option>
+                    <option value='health'>Health</option>
+                    <option value='insurance'>Insurance</option>
+                    <option value='entertainment'>Entertainment</option>
+                    <option value='gifts'>Gifts</option>
+                    <option value='transport'>Transport</option>
+                    <option value='shopping'>Shopping</option>
+                  </Field>
+                  <ErrorMessage
+                    className={classes.error}
+                    component='small'
+                    name='category'
+                  />
+                </div>
+              )}
+
+              <button
+                type='submit'
+                className={`${classes.btn} ${classes.btnSubmit}`}
+              >
+                Submit
+              </button>
+            </Form>
+          )}
         </Formik>
       )}
     </Fragment>
