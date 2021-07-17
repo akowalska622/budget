d46e2b8e13b564713ee1dcd11dae486983f3f109
Anna Kowalska <a.kowalska622@gmail.com>
2021-07-17 16:17:30 +0200

    balance logic

--- src/components/Movements/Balance.js
+++ src/components/Movements/Balance.js
@@ -1,7 +1,19 @@
 import classes from './Balance.module.css';
+import { useSelector } from 'react-redux';
 
 const Balance = () => {
-  const balanceAmount = -300;
+  const movements = useSelector(state => state.movements.movements);
+  const incomes = movements
+    .filter(mov => mov.type === 'income')
+    .map(mov => mov.amount);
+
+  const expenses = movements
+    .filter(mov => mov.type === 'expense')
+    .map(mov => mov.amount * -1);
+
+  const totalMovementsWithSign = [...incomes, ...expenses];
+
+  const balanceAmount = totalMovementsWithSign.reduce((a, b) => a + b, 0);
 
   const colouring = balanceAmount > 0 ? 'green' : 'tomato';
 
